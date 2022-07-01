32fa0daaf402f1e3c1ade4cfb72ea93f465874d9
jerichosy <69558553+jerichosy@users.noreply.github.com>
2022-07-01 23:08:53 +0800

    Base front-end implementation of entry add

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,2 +1,5 @@
 $(document).ready(function () {
+    // Set default date value for the form
+    var today = new Date();
+    $('#date').val(new Date().getFullYear().toString() + '-' + (today.getMonth() + 1).toString().padStart(2, 0) + '-' + today.getDate().toString().padStart(2, 0));
 })
